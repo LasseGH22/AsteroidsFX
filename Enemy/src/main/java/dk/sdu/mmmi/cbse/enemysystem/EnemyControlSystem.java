5e35953658042eaf1819e34ce20f54a12b3dffed
Lasse Hvilsted <lahvi22@student.sdu.dk>
2024-02-18 16:23:02 +0100

    Enemy

--- Enemy/src/main/java/dk/sdu/mmmi/cbse/enemysystem/EnemyControlSystem.java
+++ Enemy/src/main/java/dk/sdu/mmmi/cbse/enemysystem/EnemyControlSystem.java
@@ -18,43 +18,39 @@ public class EnemyControlSystem implements IEntityProcessingService {
     @Override
     public void process(GameData gameData, World world) {
 
-        for (Entity player : world.getEntities(Enemy.class)) {
-            if (gameData.getKeys().isDown(GameKeys.LEFT)) {
-                player.setRotation(player.getRotation() - 5);
+
+        for (Entity enemy : world.getEntities(Enemy.class)) {
+            if (Math.random() > 0.8) {
+                if (Math.random() > 0.5) {
+                    enemy.setRotation(enemy.getRotation() - 10);
+                } else if (Math.random() > 0.5) {
+                    enemy.setRotation(enemy.getRotation() + 10);
+                }
             }
-            if (gameData.getKeys().isDown(GameKeys.RIGHT)) {
-                player.setRotation(player.getRotation() + 5);
-            }
-            if (gameData.getKeys().isDown(GameKeys.UP)) {
-                double changeX = Math.cos(Math.toRadians(player.getRotation()));
-                double changeY = Math.sin(Math.toRadians(player.getRotation()));
-                player.setX(player.getX() + changeX);
-                player.setY(player.getY() + changeY);
-            }
-            if (gameData.getKeys().isPressed(GameKeys.SPACE)) {
+
+
+
+
+            double changeX = Math.cos(Math.toRadians(enemy.getRotation()));
+            double changeY = Math.sin(Math.toRadians(enemy.getRotation()));
+            enemy.setX(enemy.getX() + changeX);
+            enemy.setY(enemy.getY() + changeY);
+
+
+            if (Math.random() > 0.975) {
+                System.out.println(Math.random());
                 for (BulletSPI bulletSPI : getBulletSPIs()) {
-                    Entity bullet = bulletSPI.createBullet(player,gameData);
+                    Entity bullet = bulletSPI.createBullet(enemy,gameData);
                     world.addEntity(bullet);
                 }
             }
 
-            if (player.getX() < 0) {
-                player.setX(1);
+            if (enemy.getX() > gameData.getDisplayWidth() || enemy.getX() + gameData.getDisplayWidth() < gameData.getDisplayWidth()) {
+                enemy.setRotation(enemy.getRotation() + 180);
             }
-
-            if (player.getX() > gameData.getDisplayWidth()) {
-                player.setX(gameData.getDisplayWidth()-1);
+            if (enemy.getY() > gameData.getDisplayHeight() || enemy.getY() + gameData.getDisplayHeight() < gameData.getDisplayHeight()) {
+                enemy.setRotation(enemy.getRotation() + 180);
             }
-
-            if (player.getY() < 0) {
-                player.setY(1);
-            }
-
-            if (player.getY() > gameData.getDisplayHeight()) {
-                player.setY(gameData.getDisplayHeight()-1);
-            }
-
-
         }
     }
 
